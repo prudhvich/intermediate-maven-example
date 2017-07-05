400225984a4871145cb59ca6d2fdbd8598bf14ac
prudhvich <chennuru.prudhvi62@gmail.com>
2017-07-05 01:30:32 -0400

    Add files via upload
    added all files

---
+++ src/test/java/com/in28minutes/maven/AppTest.java
@@ -0,0 +1,14 @@
+package com.imperio.maven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+	@Test
+	public void testApp()
+    {
+        assertEquals(0,new App().calculateSomething());
+    }
+}
